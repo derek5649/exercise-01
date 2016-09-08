31d530e09d1af25b9aa2ba2be21500329481ec6c
ironion <khanhvmai@gmail.com>
2016-09-08 18:37:56 +0000

    Add all, update.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+var celsius = prompt("Enter the temperature in Celcius", "celsius");
 
+function convertTemperature (celsius) {
+    var fahrenheit;
+    
+    // Do not modify the code above.
+    // Convert Celcius to Fahrenheit below. Fahrenheit = (Celcius * 9/5) + 32
+        fahrenheit = (celsius * 9/5) + 32;
+    
+    // Do not modify the code below.
+    return fahrenheit
+}
+
+document.getElementById("celsius").textContent = celsius;
+document.getElementById("fahrenheit").textContent = convertTemperature(celsius);
