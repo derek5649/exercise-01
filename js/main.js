104ae0e4df1f36001389ba3eeb4a5f2a115af71b
Derek Hall <dhall68@mail.valenciacollege.edu>
2016-09-21 16:24:21 +0000

    exercise-01

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+
+var name = "John";
+var firstName = "Mike";
+var surName = " Rose";
+var noun = "Hall";
+var firstName = "Mike";
+var illness = "the flue";
+var number = "90";
+var emotion = "Fear"
+var theTime = "10 AM";
+var verb = "understand";
+
+document.write("Dear " + name + ", I'm writing to inform you that "
++ firstName +  surName + " will not be attending the annual "  +  noun + " party.  "
++  firstName   + " has come down with a bad case of " + illness +
+" and is running a " + number + " degree fever.  We have an appointment with Dr. "
++ emotion + " tomorrow at " + theTime + ".  Thanks for your " + verb + "ing!" );
+
